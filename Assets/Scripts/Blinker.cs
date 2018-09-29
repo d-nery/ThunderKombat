2c6dbd6d17991cf598f5cefba70f01c80858626c
Daniel Nery <danielnso@outlook.com>
2018-09-29 00:44:00 -0300

    Start menu scenes

---
+++ Assets/Scripts/Blinker.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blinker : MonoBehaviour {
+
+    // Use this for initialization
+    void Start () {
+        InvokeRepeating("Blink", 0, 0.75f);
+    }
+
+    void Blink() {
+        if (gameObject.activeSelf)
+            gameObject.SetActive(false);
+        else
+            gameObject.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+    }
+}
