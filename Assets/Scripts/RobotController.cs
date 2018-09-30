d4f951d450c869d5ee5306eb2a8294ef5a5faa95
Erick Otsuka <erick.r.otsuka@gmail.com>
2018-09-30 08:26:50 -0300

    Change health bar background

--- Assets/Scripts/RobotController.cs
+++ Assets/Scripts/RobotController.cs
@@ -43,7 +43,7 @@ public class RobotController : MonoBehaviour {
         float v = Input.GetAxis("P" + playerNumber + "Yaxis") * motorForce;
         float h = Input.GetAxis("P" + playerNumber + "Xaxis") * motorForce;
 
-        print("v = " + v + ", h = " + h);
+        // print("v = " + v + ", h = " + h);
 
         if (Vector3.Dot(transform.up, Vector3.down) > 0) {
             if (Time.fixedTime - seconds >= 7) {
