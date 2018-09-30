64173f6cd1bb34ec24291eb1c345548f5c3b0be2
jvSanches <syxtroz@gmail.com>
2018-09-30 11:03:44 -0300

    Cheers

---
+++ Assets/cheer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cheer : MonoBehaviour {
+    private Animation animation;
+	// Use this for initialization
+	void Start () {
+        animation = GetComponent<Animation>();
+        animation.playAutomatically = false;
+        foreach (AnimationState state in animation)
+        {
+            state.speed = 1 / Time.timeScale;
+        }
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        if (Random.value < 0.005f)
+        {
+            animation.Play();
+        }
+		
+	}
+}
