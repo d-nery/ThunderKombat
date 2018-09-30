9444336bda903e1accce1f05f5e2a307b63e2ad7
Daniel Nery <danielnso@outlook.com>
2018-09-30 11:36:51 -0300

    Vs panel

---
+++ Assets/Scripts/PanelDestroy.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanelDestroy : MonoBehaviour {
+
+    // Use this for initialization
+    void Start () {
+        Invoke("noPanel", 5f);
+    }
+
+    void noPanel() {
+        Destroy(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+    }
+}
